97cfe25b4066a4f3520dabc0f796aff0c0153c7e
zeystom <gogoha.hug.8172@gmail.com>
2024-05-08 16:38:33 +0300

    Delete pythonProject4 directory

--- pythonProject4/pp.py
+++
@@ -1,8 +0,0 @@
-class PPP:
-    def __init__(self,a ,b ):
-        self.a = a
-        self.b = b
-    def square(self):
-        return self.a * self.b
-    def perim(self):
-        return self.a * 2 + self.b * 2
