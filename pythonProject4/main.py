2331397fa9a0d8b5dcce36cd582939f781630f29
zeystom <gogoha.hug.8172@gmail.com>
2024-05-08 15:14:46 +0300

    sdds

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -1,1 +1,17 @@
-print("print")
+class Calculator:
+    def __init(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def multy(self):
+        return self.a * self.b
+
+
+    def podel(self):
+        return self.a / self.b
